f8f6f3825642b1dbb141c2477519afaf48412eed
Edwin Camilo Sanabria Torres <edwin.sanabria03@uptc.edu.co>
2024-09-02 20:44:29 -0500

    cambio del validateToken 2

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -2,16 +2,24 @@ import jwt from 'jsonwebtoken';
 import { TOKEN_SECRET } from '../config.js';
 
 export const authRequired = async (req, res, next) => {
-    const token = req.headers.autentification; // Nota: asegúrate de que sea 'autentification' y no 'authorization'
+    const authHeader = req.headers.authorization;
+    console.log("Encabezados de la solicitud:", req.headers);  // Verificar los encabezados de la solicitud
+    console.log("Auth Header:", authHeader);  // Verificar el encabezado de autorización
+
+    if (!authHeader) {
+        return res.status(401).json({ message: "No token, autorización denegada" });
+    }
+
+    const token = authHeader.split(' ')[1];  // Obtener el token después de "Bearer "
     console.log("Este es el token de authrequired", token);
 
     if (!token) {
-        return res.status(401).json({ message: "No token, autorización denegada" }); // Elimina el objeto req de la respuesta
+        return res.status(401).json({ message: "No token, autorización denegada" });
     }
 
     jwt.verify(token, TOKEN_SECRET, (err, user) => {
         if (err) {
-            return res.status(403).json({ message: "Token invalido" });
+            return res.status(403).json({ message: "Token inválido" });
         }
         req.user = user;
         next();
